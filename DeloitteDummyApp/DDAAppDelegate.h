b30e7c8f8f3be62bc890e6f8f1606474cca2974a
pherring@hugeinc.com <pherring@hugeinc.com>
2013-04-17 13:57:22 -0400

    Initial Commit

---
+++ DeloitteDummyApp/DDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAAppDelegate.h
+//  DeloitteDummyApp
+//
+//  Created by HUGE | PJ Herring on 4/17/13.
+//  Copyright (c) 2013 HUGE | PJ Herring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeloitteDummyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeloitteDummyApp
+//
+//  Created by HUGE | PJ Herring on 4/17/13.
+//  Copyright (c) 2013 HUGE | PJ Herring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAAppDelegate class]));
+	}
+}
